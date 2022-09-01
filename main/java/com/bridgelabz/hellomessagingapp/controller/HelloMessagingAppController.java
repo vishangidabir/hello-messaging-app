7e92a0f4a1d34b2c561c2a2cdb792fd7179da8b0
vishangi <vishangid@gmail.com>
2022-09-01 17:27:35 +0530

    Added : Use PUT Request Method and pass first name as Path Variable and last name as Query Parameter

--- main/java/com/bridgelabz/hellomessagingapp/controller/HelloMessagingAppController.java
+++ main/java/com/bridgelabz/hellomessagingapp/controller/HelloMessagingAppController.java
@@ -7,29 +7,46 @@ import com.bridgelabz.hellomessagingapp.controller.dto.User;
 @RequestMapping("/hello")
 public class HelloMessagingAppController {
 
+    //Url : http://localhost:8080/hello/getdata
     @GetMapping("/getdata")
     public String hello() {
         return "Hello Bridgelabz!";
     }
 
+    //Url : http://localhost:8080/hello/query?name=Vishu
     @GetMapping("/query")
     public String querryParam(@RequestParam String name){
        return "Hello" + " " + name + " " + "from Bridgelabz";
     }
 
+    //Url : http://localhost:8080/hello/path/Vishu
     @GetMapping("/path/{name}")
     public String nameParam(@PathVariable String name) {
         return "Hello" + " " + name + " " + "!";
     }
 
+    //Url :  http://localhost:8080/hello/adduser
     @PostMapping("/adduser")
     public String addUser(@RequestBody User user) {
         return "Hello" + user.getFirstName() + " " + user.getLastName() +"!";
     }
 
+
+    //Url : http://localhost:8080/hello/post?firstName=Vishangi&lastName=Dabir
     @PostMapping("/post")
     public String addUserRequestParam(@RequestParam String firstName, @RequestParam String lastName) {
         User user = new User();
+        user.setFirstName(firstName);
+        user.setLastName(lastName);
+        return user.toString();
+    }
+
+    //Url : http://localhost:8080/hello/updated?firstName=Vishangi&lastName=Dabir
+    @PutMapping("/updated")
+    public String update(@RequestParam String firstName, @RequestParam String lastName) {
+        User user = new User();
+        user.setFirstName("Vishangi");
+        user.setLastName("Dabir");
         user.setFirstName(firstName);
         user.setLastName(lastName);
         return user.toString();
--- main/java/com/bridgelabz/hellomessagingapp/controller/dto/User.java
+++ main/java/com/bridgelabz/hellomessagingapp/controller/dto/User.java
@@ -27,5 +27,6 @@ public class User {
                 ", lastName='" + lastName + '\'' +
                 '}';
     }
+    
 }
 
