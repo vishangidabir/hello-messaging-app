7d3b1cb9f6ba5e7965b3abba105c73c37c29c8f5
vishangi <vishangid@gmail.com>
2022-09-01 16:00:30 +0530

    Added : Welcome to Hello Messaging App

---
+++ main/java/com/bridgelabz/hellomessagingapp/HelloMessagingAppApplication.java
@@ -0,0 +1,14 @@
+package com.bridgelabz.hellomessagingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloMessagingAppApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(HelloMessagingAppApplication.class, args);
+    }
+
+}
---
+++ test/java/com/bridgelabz/hellomessagingapp/HelloMessagingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.hellomessagingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloMessagingAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
